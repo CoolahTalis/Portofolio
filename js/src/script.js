49a1d0ee998cfe4c3ac970ec9b8d6f4fafbcc52b
CoolahTalis <c.yameogo@talis-bs.net>
2021-07-30 23:41:56 +0200

    updt design...

--- js/src/script.js
+++ js/src/script.js
@@ -1,4 +1,4 @@
-(function($) {
+// (function($) {
 
 //     let variable; // portée de block
 //     var variable2; // portée de fonction
@@ -43,10 +43,6 @@
 
 // $('dd').hide();
 
-// $('dt').on('click', function() {
-//     $('dd').not($(this).nextUntil('dt')).slideUp(100);
-//     $(this).nextUntil('dt').slideToggle(100);
-// });
 
 // $(".submit").on('click', function() {
 //     var value = $('#number').val();
@@ -54,8 +50,8 @@
     
 //     let tableau = [];
 //     for(var i=1; i<= value; i++) {
-//         if( i % 2 == 0 && choice == 'even') {
-//             tableau.push(i);
+    //         if( i % 2 == 0 && choice == 'even') {
+        //             tableau.push(i);
 //         };
 //     };
 //     console.log(tableau);
@@ -69,4 +65,64 @@
 // });
 
 
-})(jQuery);
+// $('dt').on('click', function() {
+//     $('dd').not($(this).nextUntil('dt')).slideUp(100);
+//     $(this).nextUntil('dt').slideToggle(100);
+// });
+
+
+
+// $('.demo').radiate({
+// distance: 200, // Distance from center element
+// delay: 500, // delay between each element
+// offset: -45, // offset in degree
+// angle: 'auto', // return angle in degree for each element
+// toggle:'#toggle' // jQuery selector to toggle show/hide
+// });
+
+
+// -------------------------------- Mouse Events --------------------------
+
+// const mousePointeur = document.querySelector('.mousePointeur')
+
+// window.addEventListener('mousemove', (e) => {
+//     mousePointeur.style.left = e.pageX + 'px' 
+//     mousePointeur.style.top = e.pageY + 'px'   
+// })
+
+// mousePointeur.addEventListener('mousedown', () => {
+//     mousePointeur.style.transform = 'scale(2) translate(-25%, -25%)'
+//     mousePointeur.style.transition = 'ease 0.2'
+// })
+
+// mousePointeur.addEventListener('mouseup', () => {
+//     mousePointeur.style.transform = 'scale(1) translate(-50%, -50%)'
+//     mousePointeur.style.transition = 'ease 0.2s'
+// })
+
+
+
+// -------------------------------- Page Up --------------------------
+
+const pageUp = document.querySelector('.pageUp') 
+
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY > 500) {
+        pageUp.style.visibility = 'visible'
+    }else {
+        pageUp.style.visibility = 'hidden'
+    }
+})
+
+const nav = document.querySelector('nav')
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 600) {
+        nav.style.position = 'fixed'
+    }else {
+        nav.style.position = ''
+    }
+})
+
+
+// })(jQuery);
